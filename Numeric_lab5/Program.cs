8444779da8e8bfbae50ac657252af8e36413a191
Александр Клюев <leo.sasha.20012@gmail.com>
2022-12-06 12:52:59 +0700

    fixed bugs, find boundary solutions correctly

--- Numeric_lab5/Program.cs
+++ Numeric_lab5/Program.cs
@@ -6,15 +6,17 @@ namespace Numeric_lab5
     class Program
     {
         // u = (1 + t)^3 + 2x
-        // u = t^3 + x 
-        /*public static double Uxt(double x, double t) => Math.Pow((1 + t), 3) + 2 * x;
+        public static double Uxt(double x, double t) => Math.Pow((1 + t), 3) + 2 * x;
+        public static double F(double x, double t) => 3 * Math.Pow((1 + t), 2);
         public static double Fi(double x) => 2 * x;
         public static double M1(double t) => Math.Pow((1 + t), 3);
-        public static double M2(double t) => Math.Pow((1 + t), 3) + 2;*/
-        public static double Uxt(double x, double t) => t*t*t + x;
+        public static double M2(double t) => Math.Pow((1 + t), 3) + 2;
+        
+        // u = t^3 + x 
+        /*public static double Uxt(double x, double t) => t*t*t + x;
         public static double Fi(double x) => x;
         public static double M1(double t) => t*t*t;
-        public static double M2(double t) => t*t*t+1;
+        public static double M2(double t) => t*t*t+1;*/
 
         public static void RightProgonk(int N, int M, double G)
         {
@@ -31,13 +33,15 @@ namespace Numeric_lab5
 
             for (int n = 0; n <= N; n++)
             {
-                A[0] = 0;
-                B[0] = M1(ty*n);
+                A[1] = 0;
+                B[1] = M1(ty*n);
 
                 for (int m = 1; m <= M-1; m++)
                 {
-                    f = a * Y[m - 1] - b * Y[m] + c * Y[m + 1];
-                    
+                    //f = a * Y[m-1] - b * Y[m] + c * Y[m + 1];
+
+                    f = -(h * h / ty) - (1 - G) * (Y[m - 1] - 2 * Y[m] + Y[m + 1]) - h * h * F(m * h, ty * (n + 0.5));
+
                     A[m+1] = c / (b - A[m] * a);
                     B[m+1] = (B[m] * a - f) / (b - a * A[m]);
                 }
@@ -47,28 +51,27 @@ namespace Numeric_lab5
                 {
                     Y[m - 1] = A[m] * Y[m] + B[m];
                 }
-                
-                Console.WriteLine("  m |   U[m]  |    y[m]   | esp");
-                for (int i = 0; i <= M; i++)
-                {
-                    double Ux0 = Uxt(i*h, 1);
-                    Console.WriteLine($" {i,2} |{Ux0,8:f4} | {Y[i],8:f4}  |{Math.Abs(Ux0-Y[i]):f4}");
-                }
-                Console.WriteLine("\n\n");
             }
-            
+            Console.WriteLine($"n = {N}, G = {G}");
             Console.WriteLine("  m |   U[m]  |    y[m]   | esp");
             for (int i = 0; i <= M; i++)
             {
-                double Ux0 = Uxt(i*h, 1);
-                Console.WriteLine($" {i,2} |{Ux0,8:f4} | {Y[i],8:f4}  |{Math.Abs(Ux0-Y[i]):f4}");
+                double Ux1 = Uxt(i*h, 1);
+                Console.WriteLine($" {i,2} |{Ux1,8:f4} | {Y[i],8:f4}  |{Math.Abs(Ux1-Y[i]):f4}");
             }
+            Console.WriteLine("\n\n");
 
         }
         
         static void Main(string[] args)
         {
-            RightProgonk(10,10,1);
+            RightProgonk(10, 10, 0.2);
+            RightProgonk(10, 10, 0.5);
+            RightProgonk(10, 10, 1);
+            RightProgonk(15, 10, 1);
+            RightProgonk(20, 10, 1);
+            RightProgonk(25, 10, 1);
+            RightProgonk(35, 35, 1);
         }
     }
 }
--- Numeric_lab5/obj/Release/net5.0/Numeric_lab5.AssemblyInfo.cs
+++ Numeric_lab5/obj/Release/net5.0/Numeric_lab5.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("Numeric_lab5")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Numeric_lab5")]
