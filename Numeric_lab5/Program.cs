62fd56d6830b0e9e250f71a3aaf528e8922529d7
Александр Клюев <leo.sasha.20012@gmail.com>
2022-12-13 14:43:16 +0700

    done

--- Numeric_lab5/Program.cs
+++ Numeric_lab5/Program.cs
@@ -5,18 +5,21 @@ namespace Numeric_lab5
 {
     class Program
     {
+        //N >= 2M*M
+        
         // u = (1 + t)^3 + 2x
         public static double Uxt(double x, double t) => Math.Pow((1 + t), 3) + 2 * x;
         public static double F(double x, double t) => 3 * Math.Pow((1 + t), 2);
         public static double Fi(double x) => 2 * x;
         public static double M1(double t) => Math.Pow((1 + t), 3);
         public static double M2(double t) => Math.Pow((1 + t), 3) + 2;
-        
+
         // u = t^3 + x 
-        /*public static double Uxt(double x, double t) => t*t*t + x;
-        public static double Fi(double x) => x;
-        public static double M1(double t) => t*t*t;
-        public static double M2(double t) => t*t*t+1;*/
+        //public static double Uxt(double x, double t) => t*t*t + x;
+        //public static double F(double x, double t) => 3 * t * t;
+        //public static double Fi(double x) => x;
+        //public static double M1(double t) => t*t*t;
+        //public static double M2(double t) => t*t*t+1;
 
         public static void RightProgonk(int N, int M, double G)
         {
@@ -38,8 +41,6 @@ namespace Numeric_lab5
 
                 for (int m = 1; m <= M-1; m++)
                 {
-                    //f = a * Y[m-1] - b * Y[m] + c * Y[m + 1];
-
                     f = -(h * h / ty)* Y[m] - (1 - G) * (Y[m - 1] - 2 * Y[m] + Y[m + 1]) - h * h * F(m * h, ty * (n + 0.5));
 
                     A[m+1] = c / (b - A[m] * a);
@@ -52,8 +53,8 @@ namespace Numeric_lab5
                     Y[m - 1] = A[m] * Y[m] + B[m];
                 }
             }
-            Console.WriteLine($"n = {N}, m = {M}, G = {G}, max esp = {Enumerable.Range(0,M+1).Select(i => Math.Abs(Uxt(i*h,1)-Y[i])).Max():f4}" );
-            Console.WriteLine("  m |   U[m]  |    y[m]   | esp");
+            Console.WriteLine($"n = {N}, m = {M}, G = {G}, max eps = {Enumerable.Range(0,M+1).Select(i => Math.Abs(Uxt(i*h,1)-Y[i])).Max():f4}" );
+            Console.WriteLine("  m |   U[m]  |    y[m]   | eps");
             for (int i = 0; i <= M; i++)
             {
                 double Ux1 = Uxt(i*h, 1);
@@ -65,6 +66,8 @@ namespace Numeric_lab5
         
         static void Main(string[] args)
         {
+            //N >= 2M*M
+
             RightProgonk(10, 10, 0.2);
             RightProgonk(10, 10, 0.5);
             RightProgonk(10, 10, 1);
@@ -72,6 +75,7 @@ namespace Numeric_lab5
             RightProgonk(20, 10, 1);
             RightProgonk(25, 10, 1);
             RightProgonk(35, 35, 1);
+            RightProgonk(8000, 20, 0.1);
         }
     }
 }
